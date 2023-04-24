6c6adb8836a452777ac12e9e909c1a18f9d46ca4
shaigue <shaigue@gmail.com>
2023-04-24 17:01:00 +0300

    count_ngrams_in_batches.py refactored, unit-tested and documented

--- src/count_ngrams_in_batches.py
+++ src/count_ngrams_in_batches.py
@@ -5,7 +5,6 @@ import sys
 from collections import Counter
 import logging
 from pathlib import Path
-from typing import Any
 
 import psutil
 from datasets import Dataset as HuggingfaceDataset
@@ -17,33 +16,21 @@ from src.get_tokenizer import get_tokenizer
 from src.load_dataset import load_bookcorpus_dataset
 
 MEGA = 2 ** 20
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s',
+                    filename='../logs/count_ngram_in_batches.log', filemode='a')
 
 
-# TODO: update/write docstring
-# TODO: figure how do I want to do logging
+# TODO: write docstring
+# TODO: write an end-to-end test that checks that everything works right.
+#   this can be done after the module for integrating the file with DuckDB is done.
+# TODO: create a main script that runs this together with the other parts.
+# TODO: analyze the results, and try to interpolate to the entire dataset
+# TODO: add checkpoints? so we can resume if we where interrupted?
+# TODO: figure how do I want to do logging. maybe log details about the dataset? or something else?
+# TODO: maybe automatically delete the files when re-starting?
 
 
-def __count_ngrams_in_batch(batch: list[list[int]], max_ngram_size: int) -> \
-        dict[int, Counter[tuple[int, ...], int]]:
-    """Counts how many times each n-gram appears in a batch of sequences,
-    and how many n-grams appear in the batch, by n-gram size.
-    :param batch: a batch of token sequences.
-    :param max_ngram_size: count n-grams up to this size.
-    :return: a Counter mapping an n-gram to the number of times it appears in the batch.
-    """
-    ngram_size_to_counter = {}
-    for ngram_size in range(1, max_ngram_size + 1):
-        counter = Counter()
-        for sequence in batch:
-            for start_i in range(len(sequence) - ngram_size + 1):
-                ngram = tuple(sequence[start_i:start_i + ngram_size])
-                counter[ngram] += 1
-        ngram_size_to_counter[ngram_size] = counter
-    return ngram_size_to_counter
-
-
-def __get_memory_stats_mb() -> dict:
+def get_memory_stats_mb() -> dict:
     mem = psutil.virtual_memory()
     return {
         'total': mem.total // MEGA,
@@ -52,8 +39,8 @@ def __get_memory_stats_mb() -> dict:
     }
 
 
-def __tokenize_dataset(dataset: HuggingfaceDataset, tokenizer: PreTrainedTokenizerBase,
-                       n_workers: int, tokenizer_batch_size: int):
+def tokenize_dataset(dataset: HuggingfaceDataset, tokenizer: PreTrainedTokenizerBase,
+                     n_workers: int, tokenizer_batch_size: int):
     def tokenize(batch: dict[str, list]):
         return tokenizer(batch['text'], add_special_tokens=False)
 
@@ -66,55 +53,99 @@ def __tokenize_dataset(dataset: HuggingfaceDataset, tokenizer: PreTrainedTokeniz
     return dataset
 
 
-def __count_total_ngrams_of_size(dataset: HuggingfaceDataset, max_ngram_size: int) \
-        -> dict[int, int]:
-    input_ids = dataset.data.column('input_ids')
-    sequence_lengths = pa.compute.list_value_length(input_ids)
+def count_ngrams_in_batch(batch: list[list[int]], max_ngram_size: int) -> dict[int, Counter[tuple[int, ...], int]]:
+    """Counts how many times each ngram appears in the batch.
+    :param batch: a list of token sequences.
+    :param max_ngram_size: the maximum size of ngrams to consider.
+    :returns: a dictionary mapping from ngram sizes to the counter of ngrams of that size.
+        Each counter is a mapping from ngrams (tuple of ints) to the number of times it appears in the batch.
+    """
+    ngram_size_to_counter = {}
+    for ngram_size in range(1, max_ngram_size + 1):
+        counter = Counter()
+        for sequence in batch:
+            for start_i in range(len(sequence) - ngram_size + 1):
+                ngram = tuple(sequence[start_i:start_i + ngram_size])
+                counter[ngram] += 1
+        ngram_size_to_counter[ngram_size] = counter
+    return ngram_size_to_counter
 
+
+def count_total_ngrams_of_size_(input_ids: pa.Array, max_ngram_size: int):
+    """Helper function for `count_total_ngrams_of_size` for testing."""
+    sequence_lengths = pa.compute.list_value_length(input_ids)
     total_ngrams_of_size = {}
     for ngram_size in range(1, max_ngram_size + 1):
         subtract_constant = ngram_size - 1
         total_ngrams_of_size_per_sample = pa.compute.subtract(sequence_lengths, subtract_constant)
         total_ngrams_of_size_per_sample = pa.compute.max_element_wise(total_ngrams_of_size_per_sample, 0)
         total_ngrams_of_size[ngram_size] = pa.compute.sum(total_ngrams_of_size_per_sample).as_py()
-
     return total_ngrams_of_size
 
 
-def __count_ngrams_in_batches_and_save_to_file(dataset: HuggingfaceDataset, n_workers: int,
-                                               ngram_count_batch_size: int, max_ngram_size: int,
-                                               filter_ngram_count_threshold: int, save_dir: Path) -> None:
+def count_total_ngrams_of_size(dataset: HuggingfaceDataset, max_ngram_size: int) -> dict[int, int]:
+    """Counts the total number of ngrams of every size up to `max_ngram_size`.
+    :param dataset: the tokenized dataset (assumed tokens are available in the column `input_ids`
+    :param max_ngram_size: the maximal ngram size to be counted.
+    :returns: a dictionary mapping ngram size to the total number of ngrams of that size.
+    """
+    # TODO: will this work when the dataset is larger than memory? I'm not sure. need to test that.
+    #   if so we might need to chunk it and aggregate over the chunks
+    input_ids = dataset.data.column('input_ids')
+    return count_total_ngrams_of_size_(input_ids, max_ngram_size)
+
+
+def convert_ngram_counter_to_pa_table(counter: Counter[tuple[int, ...], int], ngram_size: int,
+                                      filter_ngram_count_threshold: int) -> pa.Table:
+    """Converts a counter containing counts of ngrams of a given size to a pyarrow table to save it in parquet format.
+    :param counter: a mapping between an ngram to its count
+    :param ngram_size: the size of the ngrams in this counter
+    :param filter_ngram_count_threshold: only keep ngrams that have counts greater or equal to this threshold
+    :returns: a pyarrow table with columns 'token_1', 'token_2', ... (depending on `ngram_size`) and 'count'.
+    """
+    token_columns = [[] for _ in range(ngram_size)]
+    token_columns_names = [f'token_{i}' for i in range(1, ngram_size + 1)]
+    count_column = []
+    for ngram, count in counter.items():
+        if count < filter_ngram_count_threshold:
+            continue
+        for i, token in enumerate(ngram):
+            token_columns[i].append(token)
+        count_column.append(count)
+    table = pa.table(
+        [*token_columns, count_column],
+        names=[*token_columns_names, 'count']
+    )
+    return table
+
+
+def count_ngrams_in_batches_and_save_to_file(dataset: HuggingfaceDataset, n_workers: int,
+                                             ngram_count_batch_size: int, max_ngram_size: int,
+                                             filter_ngram_count_threshold: int, save_dir: Path) -> None:
+    """Splits the dataset into batches and counts ngrams in each batch. saves result for each batch in a file.
+    Resulting files are saved in `save_dir` in a separate subdirectory for each ngram size.
+    :param dataset: the dataset to process. assumes that it is already tokenized.
+    :param n_workers: number of worker processes
+    :param ngram_count_batch_size: the size of the batch to split the dataset into
+    :param max_ngram_size: maximum ngram size to consider
+    :param filter_ngram_count_threshold: only ngrams with counts (per batch) greater or equal than this threshold will
+        be saved.
+    :param save_dir: directory to save the resulting files
+    """
     def count_ngrams_in_batch_and_save_to_file(batch: dict[str, list], indices: list[int]) -> None:
         start, end = indices[0], indices[-1]
-        logging.info(f'started working on samples {start}-{end};'
-                     f'memory: {__get_memory_stats_mb()}')
-        ngram_size_to_counter = __count_ngrams_in_batch(
-            batch['input_ids'],
-            max_ngram_size,
-        )
-        for ngram_size, counter in ngram_size_to_counter.items():
-            token_columns = [[] for _ in range(ngram_size)]
-            token_columns_names = [f'token_{i}' for i in range(1, ngram_size + 1)]
-            count_column = []
-            for ngram, count in counter.items():
-                if count < filter_ngram_count_threshold:
-                    continue
-                for i, token in enumerate(ngram):
-                    token_columns[i].append(token)
-                count_column.append(count)
+        logging.info(f'started working on samples {start}-{end};memory (MB): {get_memory_stats_mb()}')
+        ngram_size_to_counter = count_ngrams_in_batch(batch['input_ids'], max_ngram_size)
 
-            table = pa.table(
-                [*token_columns, count_column],
-                names=[*token_columns_names, 'count']
-            )
+        for ngram_size, counter in ngram_size_to_counter.items():
+            table = convert_ngram_counter_to_pa_table(counter, ngram_size, filter_ngram_count_threshold)
             save_dir_per_size = save_dir / str(ngram_size)
             save_dir_per_size.mkdir(parents=True, exist_ok=True)
             path = save_dir_per_size / f'count_table_{start}-{end}.parquet'
             pq.write_table(table, str(path))
 
-        logging.info(f'finished samples {start} to {end};'
-                     f'memory: {__get_memory_stats_mb()};'
-                     f'counter size: {sys.getsizeof(ngram_size_to_counter) // MEGA}')
+        logging.info(f'finished samples {start} to {end};memory (MB): {get_memory_stats_mb()};'
+                     f'counter size (MB): {sys.getsizeof(ngram_size_to_counter) // MEGA}')
 
     dataset.map(
         count_ngrams_in_batch_and_save_to_file,
@@ -129,8 +160,21 @@ def count_ngrams_in_batches(dataset: HuggingfaceDataset, tokenizer: PreTrainedTo
                             save_dir: Path, tokenizer_batch_size: int = 4_000,
                             ngram_count_batch_size: int = 200_000, n_samples: int = None,
                             n_workers: int = None, max_ngram_size: int = 5,
-                            filter_ngram_count_threshold: int = 0) -> None:
-    # TODO: maybe add logging on the name of the dataset? or somewhere else?
+                            filter_ngram_count_threshold: int = 1) -> None:
+    """Main function for this module. gets a dataset, tokenizes it, counts how many times each ngram appears in the
+    dataset and the total number of ngrams of each size are in the dataset, and saves those to files.
+    :param dataset: dataset to process. assume it contains text in the 'text' column
+    :param tokenizer: tokenizer to use for tokenizing the dataset.
+    :param save_dir: directory to save the resulting files
+    :param tokenizer_batch_size: the batch size to use when tokenizing
+    :param ngram_count_batch_size: the batch size to use when counting ngrams
+    :param n_samples: number of samples to take from the start of the dataset.
+        If this is None, the entire dataset will be processed.
+    :param n_workers: the number of worker processes to use.
+    :param max_ngram_size: maximal size of ngrams to count
+    :param filter_ngram_count_threshold: only ngrams with counts (per batch) greater or equal to this threshold will be
+        saved.
+    """
     logging.info('Starting to count ngrams in batches')
     save_dir.mkdir(parents=True, exist_ok=True)
     if n_workers is None:
@@ -138,21 +182,19 @@ def count_ngrams_in_batches(dataset: HuggingfaceDataset, tokenizer: PreTrainedTo
     if n_samples is not None:
         dataset = dataset.select(range(n_samples))
 
-    dataset = __tokenize_dataset(dataset, tokenizer, n_workers, tokenizer_batch_size)
+    dataset = tokenize_dataset(dataset, tokenizer, n_workers, tokenizer_batch_size)
 
     ngram_of_size_file = save_dir / 'total_ngrams_per_size.json'
-    total_ngrams_per_size = __count_total_ngrams_of_size(dataset, max_ngram_size, n_workers)
+    total_ngrams_per_size = count_total_ngrams_of_size(dataset, max_ngram_size)
     with ngram_of_size_file.open('w') as f:
         json.dump(total_ngrams_per_size, f, indent=4)
 
-    __count_ngrams_in_batches_and_save_to_file(dataset, n_workers, ngram_count_batch_size,
-                                               max_ngram_size, filter_ngram_count_threshold, save_dir)
+    count_ngrams_in_batches_and_save_to_file(dataset, n_workers, ngram_count_batch_size,
+                                             max_ngram_size, filter_ngram_count_threshold, save_dir)
     logging.info('Finished counting ngrams in batches')
 
 
 if __name__ == '__main__':
-    # TODO: write an end-to-end test that checks that everything works right.
-    #   this can be done after the module for integrating the file with DuckDB is enabled.
     dataset = load_bookcorpus_dataset()
     tokenizer = get_tokenizer()
     save_dir = Path('../data')
@@ -161,9 +203,9 @@ if __name__ == '__main__':
         dataset=dataset,
         tokenizer=tokenizer,
         save_dir=save_dir,
-        ngram_count_batch_size=100_000,
-        n_samples=1_000_000,
-        n_workers=2,
+        ngram_count_batch_size=1_000_000,
+        n_samples=20_000_000,
+        n_workers=3,
         max_ngram_size=5,
-        filter_ngram_count_threshold=0,
+        filter_ngram_count_threshold=2,
     )
--- tests/test_count_ngrams_in_batches.py
+++ tests/test_count_ngrams_in_batches.py
@@ -1,11 +1,82 @@
 import unittest
+from collections import Counter
+
+import pyarrow as pa
+
+from src.count_ngrams_in_batches import count_ngrams_in_batch, convert_ngram_counter_to_pa_table, \
+    count_total_ngrams_of_size_
 
 
-class MyTestCase(unittest.TestCase):
-    # TODO: test counting ngrams in a batch
-    # TODO: test counting total_ngrams_of_size
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestCountNgramsInBatches(unittest.TestCase):
+    def test_count_ngrams_in_batch(self):
+        batch = [
+            [1, 1, 1, 0, 1, 0, 2, 0, 2, 1]
+        ]
+        max_ngram_len = 3
+        expected = {
+            1: {
+                (1,): 5,
+                (0,): 3,
+                (2,): 2,
+            },
+            2: {
+                (0, 1): 1,
+                (1, 0): 2,
+                (1, 1): 2,
+                (0, 2): 2,
+                (2, 0): 1,
+                (2, 1): 1,
+            },
+            3: {
+                (1, 1, 1): 1,
+                (1, 1, 0): 1,
+                (1, 0, 1): 1,
+                (0, 1, 0): 1,
+                (1, 0, 2): 1,
+                (0, 2, 0): 1,
+                (2, 0, 2): 1,
+                (0, 2, 1): 1
+            },
+        }
+        result = count_ngrams_in_batch(batch, max_ngram_len)
+        self.assertEqual(expected, result)
+
+    def test_convert_ngram_counter_to_pa_table(self):
+        counter = Counter({
+            (1, 1, 1): 1,
+            (1, 1, 0): 2,
+            (1, 0, 1): 1,
+            (0, 1, 0): 3,
+            (1, 0, 2): 1,
+            (0, 2, 0): 4,
+        })
+        ngram_size = 3
+        filter_ngram_count_threshold = 1
+        result = convert_ngram_counter_to_pa_table(counter, ngram_size, filter_ngram_count_threshold)
+        expected = pa.table(
+            data={
+                'token_1': [1, 1, 1, 0, 1, 0],
+                'token_2': [1, 1, 0, 1, 0, 2],
+                'token_3': [1, 0, 1, 0, 2, 0],
+                'count':   [1, 2, 1, 3, 1, 4]
+            }
+        )
+        self.assertEqual(expected, result)
+
+    def test_count_total_ngrams_of_size_(self):
+        input_ids = pa.array([
+            [1, 2, 3, 4],
+            [1],
+            [1, 2, 3, 4, 5, 6]
+        ])
+        max_ngram_size = 3
+        expected = {
+            1: 4 + 1 + 6,
+            2: 3 + 0 + 5,
+            3: 2 + 0 + 4,
+        }
+        result = count_total_ngrams_of_size_(input_ids, max_ngram_size)
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
