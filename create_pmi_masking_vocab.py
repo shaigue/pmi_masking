0116152e4569479e18f2dd2fdd4961b6ebf69af8
shaigue <shaigue@gmail.com>
2023-06-12 12:16:03 +0300

    added input validation in entrence script

--- create_pmi_masking_vocab.py
+++ create_pmi_masking_vocab.py
@@ -1,4 +1,5 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace, ArgumentTypeError
+from collections.abc import Callable
 from importlib import import_module
 
 import config
@@ -21,132 +22,137 @@ def get_experiment_config_options() -> list[str]:
     return config_name_options
 
 
-def get_argument_parser() -> ArgumentParser:
+def get_parser() -> ArgumentParser:
     parser = ArgumentParser()
-    # TODO: I want all arguments to be full named arguments. I think that
-    #   this is the most clear way to write that.
-    # TODO: sort out required and not required arguments.
-    # TODO: set up default values
-    # TODO: make sure to do input validity checks
-    # TODO: make sure to add help for each of the arguments
-    # TODO: consider using a unified model for the return type
-    # TODO: standardize the args somehow and pass to logging & running
-    #  I think that simplest way to achieve that is to just pass the arguments
-    #  directly, without having a special class for that. the description will
-    #  appear in the documentation + in here
-    # TODO: make sure to organize the arguments such that required arguments appear first
-    # action: store, store_true, store_false, append
-    # nargs: N, ?, *, +
-    # default:
-    # type:
-    # choices:
-    # required: True/False
-    # help
-    # TODO: create an input validation function.
-    # TODO: When doing input validation, use a separate function, and raise ArgumentError
-    #
+    # TODO: pass arguments down the line explicitly. don't use a special config dataclass. I think this is simplest.
+
+    # methods for input validation.
+    def min_value_int_type(min_value: int) -> Callable[[str], int]:
+        def min_value_int(s: str) -> int:
+            i = int(s)
+            if i < min_value:
+                raise ArgumentTypeError(f'should be >= {min_value}')
+            return i
+        return min_value_int
+    positive_int = min_value_int_type(1)
+
+    def positive_float(s: str) -> float:
+        f = float(s)
+        if f <= 0:
+            raise ArgumentTypeError(f'should be strictly positive')
+        return f
+
     parser.add_argument(
         '--experiment_name',
-        help='Name of the experiment. Affects logging and resulting file names.',
+        help='experiment name. affects logging and resulting file names',
         type=str,
         required=True
     )
-    # TODO: the choices in here should probably come from the load_dataset module, and
-    #   not be duplicated here.
+    # TODO: choices here should come from the `load_dataset` module, and not duplicated here.
     parser.add_argument(
         '--dataset_name',
-        help='Which dataset to use.',
+        help='determines which dataset to use',
         type=str,
         choices=['bookcorpus', 'wikipedia', 'bookcorpus+wikipedia'],
         required=True
     )
-
+    # TODO: choices here should come from `load_tokenizer` module, and not duplicated here.
     parser.add_argument(
         '--tokenizer_name',
-        help='Name of the tokenizer to use.',
+        help='tokenizer to use',
         type=str,
         choices=['default'],
         default='default',
     )
-    # TODO: validate that this is a positive number, larger than 1.
     parser.add_argument(
         '--max_ngram_size',
-        help='Maximal ngram size to consider.',
-        type=int,
+        help='maximum ngram size to consider',
+        type=min_value_int_type(2),
         default=5,
     )
-    # TODO: validate this is a positive number, and is >= from the batch version.
     parser.add_argument(
         '--min_count_threshold',
-        help='Prunes ngrams that appear less than this amount in the entire dataset.',
-        type=int,
+        help='prunes ngrams that appear less than this amount in the entire dataset',
+        type=positive_int,
         default=10,
     )
-    # TODO: validate a positive number
     parser.add_argument(
         '--vocab_size',
-        help='Number of ngrams (excluding unigrams) to select for the PMI masking vocabulary.',
-        type=int,
+        help='number of ngrams (excluding unigrams) to select for the PMI masking vocabulary',
+        type=positive_int,
         default=800_000,
     )
-    # TODO: figure out what is a good way to pass this type of parameter
-    # TODO: validate that it sums up to 100
-    # TODO: validate that each ngram size is specified
-    # TODO: this should receive a list, where the percent are ordered (first is ngrams
-    #  of size 2, size 3, size 4, ...
-    # TODO: add example for all of the arguments in the help.
+    # TODO: I changed the format from dictionary to list. make sure down the line
+    #   is aware of that.
     parser.add_argument(
         '--ngram_size_to_vocab_percent',
-        help='Percentage of ngram size to include in the resulting vocabulary. '
-             'Expected a list of values, one for each ngram size from 2 to `max_ngram_size`. '
-             'For example, `--ngram_size_to_vocab_percent 50 25 12.5 12.5 means '
-             'that the resulting vocabulary will contain 50% ngrams of size 2, 25% ngrams of '
-             'size 3, 12.5% ngrams of size 4 and 12.5% ngrams of size 5. '
-             'Note that values should sum up to 100% and every ngram should get a positive value.',
+        help='percentage of ngram size to include in the resulting vocabulary. '
+             'this should be a list of values, one for each ngram size, from 2 to `max_ngram_size`. '
+             'for example, `--ngram_size_to_vocab_percent 50 25 12.5 12.5` means '
+             'that the resulting vocabulary will contain 50%% ngrams of size 2, 25%% ngrams of '
+             'size 3, 12.5%% ngrams of size 4 and 12.5%% ngrams of size 5. '
+             'values should sum up to 100%% and every ngram should get a positive value',
         nargs='+',
-        type=float,
+        type=positive_float,
         default=[50., 25., 12.5, 12.5],
     )
-    # TODO: validate positive value
     parser.add_argument(
         '--ngram_count_batch_size',
-        help='Ngrams are first counted in batches instead of the entire dataset, '
+        help='ngrams are first counted in batches instead of the entire dataset, '
              'for parallelization. '
-             'This value specifies how many samples from the dataset goes to each batch. '
-             'If the value is too high, counts will not fit to memory and this might slow the program, '
-             'low values can also slow the program down, as it will introduce a lot of context switches.',
-        type=int,
+             'this is the number of samples that goes into each batch. '
+             'if value is too high, counts will not fit into memory and this will slow the program. '
+             'low values will create a lot of context switches and will also slow down the program',
+        type=positive_int,
         default=1_000_000
     )
-    # TODO: validate positive number
-    parser.add_argument(
-        '--n_workers',
-        help='Number of workers to use. Defaults to the number of available CPUs.',
-        type=int,
-        default=get_available_cpus_count()
-    )
-    # TODO: validate positive value smaller than `min_count_batch`
     parser.add_argument(
         '--min_count_batch_threshold',
-        help='Ngrams that occur less than this amount in a batch will be pruned from '
-             'this batch counts. Value of 1 means that all the ngrams that appear in a batch '
+        help='ngrams that occur less than this amount in a batch will be pruned from '
+             'that batch counts. value of 1 means that all the ngrams that appear in a batch '
              'will be counted, and value of 2 means that ngrams that appear only once in a batch '
-             'will not be counted in that batch. Since most ngrams appear once, using a value of '
-             '2 or greater can greatly reduce space and time requirements.',
-        type=int,
+             'will be pruned from that batch counts. since most ngrams appear once, using a value >= 2 '
+             'can greatly reduce space and time requirements',
+        type=positive_int,
         default=1
     )
-    # TODO: this argument is for experimentation purposes. move it down.
-    # TODO: validate inputs
+    parser.add_argument(
+        '--n_workers',
+        help='number of workers to use. defaults to the number of available CPUs',
+        type=positive_int,
+        default=get_available_cpus_count()
+    )
     parser.add_argument(
         '--n_samples',
-        help='If provided, the program will only use the first `n_samples` samples of '
-             'the dataset. If not provided, the program will use the entire dataset. '
-             'This argument is mainly for testing and experimentation purposes.',
-        type=int,
+        help='if provided, only the first `n_samples` samples of '
+             'the dataset will be used. if not, the entire dataset will be used. '
+             'This argument is for testing and experimentation purposes',
+        type=positive_int,
         default=None,
     )
     return parser
+
+
+def validate_arguments(parser: ArgumentParser, args: Namespace) -> None:
+    ngram_size_to_vocab_percent = args.ngram_size_to_vocab_percent
+    expected_len = args.max_ngram_size - 1
+    if len(args.ngram_size_to_vocab_percent) != expected_len:
+        parser.error(f'ngram_size_to_vocab_percent should be of length {expected_len}, got length '
+                     f'{len(ngram_size_to_vocab_percent)}')
+    if sum(ngram_size_to_vocab_percent) != 100:
+        parser.error(f'ngram_size_to_vocab_percent should sum to 100. {ngram_size_to_vocab_percent} '
+                     f'sums to {sum(ngram_size_to_vocab_percent)}')
+    if args.min_count_threshold <= args.min_count_batch_threshold:
+        parser.error(f'min_count_threshold should be greater than min_count_batch_threshold. '
+                     f'got min_count_threshold={args.min_count_threshold} and '
+                     f'min_count_batch_threshold={args.min_count_batch_threshold}')
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+    validate_arguments(parser, args)
+    print(args)
 
 
 def old_main():
@@ -159,16 +165,6 @@ def old_main():
     experiment_config = import_module(f'experiment_config.{experiment_config_name}').config
     run_pipeline(experiment_config)
     logger.info(f'end experiment_config: {experiment_config_name}')
-
-
-def main():
-    parser = get_argument_parser()
-    args = parser.parse_args()
-    print(args)
-
-    # logger.info(f'start experiment_config: {experiment_config_name}')
-    # run_pipeline(experiment_config)
-    # logger.info(f'end experiment_config: {experiment_config_name}')
 
 
 if __name__ == '__main__':
