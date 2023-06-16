a3dd04043cb310e971cd79e5b8cc570580f5437d
shaigue <shaigue@gmail.com>
2023-06-16 13:09:06 +0300

    converted ExperimentConfig objects to bash scripts

--- experiment_config/medium_size_bookcorpus.py
+++
@@ -1,8 +0,0 @@
-"""Configuration for generating PMI vocabulary for a medium size (~30%) subset of Bookcorpus dataset."""
-from src.experiment_config import ExperimentConfig
-
-config = ExperimentConfig(
-    name='medium_size_bookcorpus',
-    n_samples=30_000_000,
-    min_count_batch_threshold=2,
-)
